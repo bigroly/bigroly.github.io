a832335cf848c166a51a59140ce4475b2e3e5e56
Rolando Chancellor <chancellor.rolando@gmail.com>
2023-05-07 23:36:15 +1000

    add auth service to portfolio

--- site.js
+++ site.js
@@ -1,3 +1,117 @@
+const devWork = [
+  {
+    title: 'RolyApps Authentication Service Template',
+    description: `Almost anything you want to build requires authentication of some sort, so why not centralise it for easy reusability moving forward.
+                  I'm hoping to tinker with other new frameworks and technologies in the future by creating simple applications to get experience with them, the first step
+                  was to get a fairly lightweight authentication service up to support this, which is where this project comes in. Check out the Readme in repo for more details!`,
+    imgLink: 'img/dev-6-auth.PNG',
+    links: [
+      {
+        url: 'https://github.com/bigroly/roly-auth-backend',
+        faIcon: 'fab fa-github',
+        text: 'View on Github',
+      }
+    ],
+    badges: [
+      {
+        type: 'badge-info',
+        text: 'C# (.Net)'
+      },
+      {
+        type: 'badge-warning',
+        text: 'AWS Cognito'
+      },
+      {
+        type: 'badge-warning',
+        text: 'Lambda'
+      },
+      {
+        type: 'badge-warning',
+        text: 'API Gateway'
+      }
+    ]
+  },
+  {
+    title: 'NgRx Demo',
+    description: `Had never tried any formal app-level state management for Angular before so I thought I would give NgRx a try. 
+                  This a basic implementation to pull user info from a mock endpoint. Will look to update and point it towards Firebase for something in the future.`,
+    imgLink: 'img/dev-5-ngrx.PNG',
+    links: [
+      {
+        url: 'https://github.com/bigroly/ngrx-demo',
+        faIcon: 'fab fa-github',
+        text: 'View on Github',
+      }
+    ],
+    badges: [
+      {
+        type: 'badge-danger',
+        text: 'Angular'
+      },
+      {
+        type: 'badge-info',
+        text: 'NgRx'
+      }
+    ]
+  },
+  {
+    title: 'Vue State',
+    description: `First attempt at a Vue app integrating application level state management using
+                  VueX.This builds on the previous app I created, again using the super simple
+                  use-case of a
+                  To-do list, but this time adding Vuex to manage component state at an app level
+                  supporting CRUD functionality,
+                  rather than passing props up and down the component chain.`,
+    imgLink: 'img/dev-1-vuestate.png',
+    links: [
+      {
+        url: 'https://github.com/bigroly/vue-state',
+        faIcon: 'fab fa-github',
+        text: 'View on Github',
+      }
+    ],
+    badges: [
+      {
+        type: 'badge-success',
+        text: 'VueJS'
+      },
+      {
+        type: 'badge-info',
+        text: 'Vue CLI'
+      },
+      {
+        type: 'badge-info',
+        text: 'VueX'
+      }
+    ]
+  },
+  {
+    title: 'Between The Ears',
+    description: `University project where we built a system for BTE - a sports psychology practice in Melbourne.
+                  This project was built using CakePHP <sup>pls don't judge me, we were forced to do PHP :'( </sup> and included:
+                  <ul class="">
+                    <li>Customer self-serve appointment booking and invoice generation</li>
+                    <li>Configurable online store, linked to Paypal for payments.</li>
+                    <li>Public facing website driven by a CMS.</li>
+                    <li>Blog post and subscription functionality.</li>
+                  </ul>`,
+    imgLink: 'img/dev-4-bte.PNG',
+    links: [
+      {
+        url: 'https://www.betweentheears.com.au/home',
+        faIcon: 'fas fa-external-link-alt',
+        text: 'View Website',
+      }
+    ],
+    badges: [
+      {
+        type: 'badge-info',
+        text: 'PHP'
+      }
+    ]
+  }
+]
+
 const uxWork =[
   {
     title: 'Concept Travel App',
@@ -52,119 +166,6 @@ const uxWork =[
     ]
   }
 ]
-
-const devWork = [
-  {
-    title: 'NgRx Demo',
-    description: `Had never tried any formal app-level state management for Angular before so I thought I would give NgRx a try. 
-                  This a basic implementation to pull user info from a mock endpoint. Will look to update and point it towards Firebase for something in the future.`,
-    imgLink: 'img/dev -5-ngrx.PNG',
-    links: [
-      {
-        url: 'https://github.com/bigroly/ngrx-demo',
-        faIcon: 'fab fa-github',
-        text: 'View on Github',
-      }
-    ],
-    badges: [
-      {
-        type: 'badge-danger',
-        text: 'Angular'
-      },
-      {
-        type: 'badge-info',
-        text: 'NgRx'
-      }
-    ]
-  },
-  {
-    title: 'Vue State',
-    description: `First attempt at a Vue app integrating application level state management using
-                  VueX.This builds on the previous app I created, again using the super simple
-                  use-case of a
-                  To-do list, but this time adding Vuex to manage component state at an app level
-                  supporting CRUD functionality,
-                  rather than passing props up and down the component chain.`,
-    imgLink: 'img/dev-1-vuestate.png',
-    links: [
-      {
-        url: 'https://github.com/bigroly/vue-state',
-        faIcon: 'fab fa-github',
-        text: 'View on Github',
-      }
-    ],
-    badges: [
-      {
-        type: 'badge-success',
-        text: 'VueJS'
-      },
-      {
-        type: 'badge-info',
-        text: 'Vue CLI'
-      },
-      {
-        type: 'badge-info',
-        text: 'VueX'
-      }
-    ]
-  },
-  {
-    title: 'Oncology Care App',
-    description: `This one came from a friend of mine whos partner worked in Oncology care.
-                  Idea was to create a medium for patients to record their ongoing treatment
-                  symptomps for efficient review at their appointments.
-                  Built as an Angular front end, integrating into a GoogleFirebase serverless back
-                  end for authentication and data storage.`,
-    imgLink: 'img/dev-2-oncologycare.PNG',
-    links: [
-      {
-        url: 'https://github.com/bigroly/CancerCareApp',
-        faIcon: 'fab fa-github',
-        text: 'View on Github',
-      }
-    ],
-    badges: [
-      {
-        type: 'badge-danger',
-        text: 'Angular'
-      },
-      {
-        type: 'badge-warning',
-        text: 'Google Firebase'
-      },
-      {
-        type: 'badge-warning',
-        text: 'Google Cloud Platform'
-      }
-    ]
-  },
-  {
-    title: 'Between The Ears',
-    description: `University project where we built a system for BTE - a sports psychology practice in Melbourne.
-                  This project was built using CakePHP and included:
-                  <ul class="">
-                    <li>Customer self-serve appointment booking and invoice generation</li>
-                    <li>Configurable online store, linked to Paypal for payments.</li>
-                    <li>Public facing website driven by a CMS.</li>
-                    <li>Blog post and subscription functionality.</li>
-                  </ul>`,
-    imgLink: 'img/dev-4-bte.PNG',
-    links: [
-      {
-        url: 'https://www.betweentheears.com.au/home',
-        faIcon: 'fas fa-external-link-alt',
-        text: 'View Website',
-      }
-    ],
-    badges: [
-      {
-        type: 'badge-info',
-        text: 'PHP'
-      }
-    ]
-  }
-]
-
 
 // {
 //   title: '',
